61abecce62c571f65b5ab27f525e109a9c3910d9
Unknown <napalm_ground@hotmail.com>
2019-04-06 20:46:54 +0200

    Setting up the box & early particle physics test

--- Assets/ParticleScript.cs
+++ Assets/ParticleScript.cs
@@ -2,18 +2,22 @@
 
 public class ParticleScript : MonoBehaviour
 {
+    public int thrust = 2000;
+
     private Rigidbody particleRigidbody;
+
 
     // Start is called before the first frame update
     void Start()
     {
         particleRigidbody = GetComponent<Rigidbody>();
-        // particleRigidbody.velocity = new Vector3(1, -1, 0);
+
+        particleRigidbody.AddForce(new Vector3(Random.value * thrust, Random.value * thrust, Random.value * thrust));
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        
     }
 }
