9f7b78c851a1f171f649e1d9313057f31926e615
Unknown <napalm_ground@hotmail.com>
2019-04-06 18:57:01 +0200

    Minor changes

---
+++ Assets/ParticleScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ParticleScript : MonoBehaviour
+{
+    private Rigidbody particleRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleRigidbody = GetComponent<Rigidbody>();
+        // particleRigidbody.velocity = new Vector3(1, -1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
